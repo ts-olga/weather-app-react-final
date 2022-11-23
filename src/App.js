c7862b10d7daefc07e25bac8883e32ab91a65d6c
Olga Tsyrulyk <olga_tsyrulyk@outlook.com>
2022-11-23 10:08:40 +0800

    Added app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Olga Tsyrulyk,{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ts-olga/weather-app-react-final"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          open-soursed on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
