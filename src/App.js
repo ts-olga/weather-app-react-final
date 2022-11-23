2b6d790b867393918946237652bcd4cd47b0f489
Olga Tsyrulyk <olga_tsyrulyk@outlook.com>
2022-11-23 10:28:50 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Olga Tsyrulyk,{" "}
         <a
           href="https://github.com/ts-olga/weather-app-react-final"
           target="_blank"
+          rel="noreferrer"
         >
           open-soursed on GitHub
         </a>
